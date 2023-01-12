9f26b874bfec345c3688707224896be7e6825f95
yuche <i@yuche.me>
2023-01-12 23:09:19 +0800

    fix: typo

--- app.config.ts
+++ app.config.ts
@@ -22,7 +22,7 @@ export default defineAppConfig({
     description: 'Developer documentation for JoyID',
     image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
     socials: {
-      github: '/nervina-labs/joyid',
+      github: 'nervina-labs/joyid',
     },
     aside: {
       level: 1,
