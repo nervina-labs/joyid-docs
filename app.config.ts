6cb51ca15100dff241d197168538f4f6f61908d6
yuche <i@yuche.me>
2023-01-12 16:03:11 +0800

    docs: add some docs

--- app.config.ts
+++ app.config.ts
@@ -1,28 +1,23 @@
 export default defineAppConfig({
   docus: {
-    title: 'Docus',
+    title: 'JoyID Docs',
     description: 'The best place to start your documentation.',
     image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
     socials: {
-      twitter: 'nuxt_themes',
-      github: 'nuxt-themes/docus',
+      github: '/nervina-labs/joyid',
     },
     aside: {
-      level: 0,
-      exclude: []
+      level: 1,
+      exclude: [],
     },
     header: {
       logo: true,
       showLinkIcon: true,
       exclude: []
     },
-    footer: {
-      iconLinks: [
-        {
-          href: 'https://nuxt.com',
-          icon: 'IconNuxtLabs'
-        }
-      ]
-    }
+    github: {
+
+    },
+    footer: {}
   }
 })
