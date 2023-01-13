3a06ce91db31f294216f898dee902d26fb915535
yuche <i@yuche.me>
2023-01-13 15:33:15 +0800

    refactor: change desc

--- app.config.ts
+++ app.config.ts
@@ -18,9 +18,7 @@ export default defineAppConfig({
   },
   docus: {
     title: 'JoyID Docs',
-    hasDocSearch: true,
     description: 'Developer documentation for JoyID',
-    image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
     socials: {
       github: 'nervina-labs/joyid',
     },
