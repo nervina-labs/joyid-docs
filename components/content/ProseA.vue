154b64d78a2f80541b78f97ece26ad9c5dd21c82
yuche <i@yuche.me>
2023-01-29 22:59:27 +0800

    feat: custom prose a

---
+++ components/content/ProseA.vue
@@ -0,0 +1,37 @@
+<script lang="ts">
+export default {
+    props: {
+        href: {
+            type: String,
+        },
+        target: {
+            type: String,
+        },
+    },
+    computed: {
+        isExternal() {
+            return this.href?.startsWith('http')
+        }
+    },
+}
+</script>
+
+<template>
+    <a v-if="isExternal" :href="href" target="_blank">
+        <slot />
+    </a>
+    <NuxtLink v-if="!isExternal" :href="href" :target="target">
+        <slot />
+    </NuxtLink>
+</template>
+
+<style scoped>
+a {
+    border-bottom: var(--prose-a-borderBottom);
+    border-style: var(--prose-a-borderStyle-default);
+    font-weight: var(--prose-a-fontWeight);
+    padding-bottom: var(--prose-a-borderDistance);
+    -webkit-text-decoration: var(--prose-a-textDecoration);
+    text-decoration: var(--prose-a-textDecoration);
+}
+</style>
